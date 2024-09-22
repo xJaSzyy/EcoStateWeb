0b482c2ab94c62ebc22a7c42752cc48f6bf73830
Novoselov Stepan <novosel.stepan@gmail.com>
2024-09-22 20:09:55 +0700

    добавил главную страницу сайта

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { createApp, createBlock } from 'vue'
 import App from './App.vue'
+import Header from './Header.vue'
 
 createApp(App).mount('#app')
+createApp(Header).mount('#header')
